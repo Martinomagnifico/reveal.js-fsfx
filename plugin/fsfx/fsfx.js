5b0374177438aa25855a1abb2761e0ea2cc2eb5b
Martino <boldie@me.com>
2022-03-19 09:19:51 +0100

    Change transition to 'slide'

--- plugin/fsfx/fsfx.js
+++ plugin/fsfx/fsfx.js
@@ -1,3 +1,4 @@
+
 /*****************************************************************
  * @author: Martijn De Jongh (Martino), martijn.de.jongh@gmail.com
  * https://github.com/Martinomagnifico
@@ -11,4 +12,401 @@
  * Thanks to:
  *  - Hakim El Hattab, Reveal.js 
  ******************************************************************/
-!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?module.exports=n():"function"==typeof define&&define.amd?define(n):(e=e||self).FsFx=n()}(this,(function(){"use strict";function e(e,t){return function(e){if(Array.isArray(e))return e}(e)||function(e,n){if("undefined"==typeof Symbol||!(Symbol.iterator in Object(e)))return;var t=[],r=!0,l=!1,o=void 0;try{for(var u,c=e[Symbol.iterator]();!(r=(u=c.next()).done)&&(t.push(u.value),!n||t.length!==n);r=!0);}catch(e){l=!0,o=e}finally{try{r||null==c.return||c.return()}finally{if(l)throw o}}return t}(e,t)||n(e,t)||function(){throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}()}function n(e,n){if(e){if("string"==typeof e)return t(e,n);var r=Object.prototype.toString.call(e).slice(8,-1);return"Object"===r&&e.constructor&&(r=e.constructor.name),"Map"===r||"Set"===r?Array.from(e):"Arguments"===r||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r)?t(e,n):void 0}}function t(e,n){(null==n||n>e.length)&&(n=e.length);for(var t=0,r=new Array(n);t<n;t++)r[t]=e[t];return r}function r(e){if("undefined"==typeof Symbol||null==e[Symbol.iterator]){if(Array.isArray(e)||(e=n(e))){var t=0,r=function(){};return{s:r,n:function(){return t>=e.length?{done:!0}:{done:!1,value:e[t++]}},e:function(e){throw e},f:r}}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var l,o,u=!0,c=!1;return{s:function(){l=e[Symbol.iterator]()},n:function(){var e=l.next();return u=e.done,e},e:function(e){c=!0,o=e},f:function(){try{u||null==l.return||l.return()}finally{if(c)throw o}}}}var l=[["requestFullscreen","exitFullscreen","fullscreenElement","fullscreenEnabled","fullscreenchange","fullscreenerror"],["webkitRequestFullscreen","webkitExitFullscreen","webkitFullscreenElement","webkitFullscreenEnabled","webkitfullscreenchange","webkitfullscreenerror"],["webkitRequestFullScreen","webkitCancelFullScreen","webkitCurrentFullScreenElement","webkitCancelFullScreen","webkitfullscreenchange","webkitfullscreenerror"],["mozRequestFullScreen","mozCancelFullScreen","mozFullScreenElement","mozFullScreenEnabled","mozfullscreenchange","mozfullscreenerror"],["msRequestFullscreen","msExitFullscreen","msFullscreenElement","msFullscreenEnabled","MSFullscreenChange","MSFullscreenError"]],o=function(){var n,t=l[0],o={},u=r(l);try{for(u.s();!(n=u.n()).done;){var c=n.value;if((null==c?void 0:c[1])in document){var i,a=r(c.entries());try{for(a.s();!(i=a.n()).done;){var s=e(i.value,2),f=s[0],d=s[1];o[t[f]]=d}}catch(e){a.e(e)}finally{a.f()}return o}}}catch(e){u.e(e)}finally{u.f()}return!1}(),u={change:o.fullscreenchange,error:o.fullscreenerror},c={request:function(){var e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:document.documentElement,n=arguments.length>1?arguments[1]:void 0;return new Promise((function(t,r){var l=function e(){c.off("change",e),t()};c.on("change",l);var u=e[o.requestFullscreen](n);u instanceof Promise&&u.then(l).catch(r)}))},exit:function(){return new Promise((function(e,n){if(c.isFullscreen){var t=function n(){c.off("change",n),e()};c.on("change",t);var r=document[o.exitFullscreen]();r instanceof Promise&&r.then(t).catch(n)}else e()}))},toggle:function(e,n){return c.isFullscreen?c.exit():c.request(e,n)},onchange:function(e){c.on("change",e)},onerror:function(e){c.on("error",e)},on:function(e,n){var t=u[e];t&&document.addEventListener(t,n,!1)},off:function(e,n){var t=u[e];t&&document.removeEventListener(t,n,!1)},raw:o};Object.defineProperties(c,{isFullscreen:{get:function(){return Boolean(document[o.fullscreenElement])}},element:{enumerable:!0,get:function(){var e;return null!==(e=document[o.fullscreenElement])&&void 0!==e?e:void 0}},isEnabled:{enumerable:!0,get:function(){return Boolean(document[o.fullscreenEnabled])}}}),o||(c={isEnabled:!1});var i=c;return function(){var e=function(e,n){var t=e.querySelectorAll(n);return Array.prototype.slice.call(t)},n=function(){return!(!i.enabled&&!i.isEnabled)},t=function(t,r){var l="BODY"==t.getRevealElement().tagName?document:t.getRevealElement(),o=e(l,".".concat(r.baseclass)),u=e(document,"[data-fs-toggle]");n()?(o.length>0?function(e){e.filter((function(e){e.style.display="inline-block";var r=function(){parseInt(e.dataset.fsGonext)>0&&!i.isFullscreen?setTimeout((function(){t.next()}),parseInt(e.dataset.fsGonext)):t.next()};e.onclick=function(){1==n()?e.hasAttribute("data-fs-gonext")?i.isFullscreen?r():(i.request(l).then(r()),element.requestFullscreen()):i.toggle(l):t.next()}}))}(o):console.log("There are no FS buttons"),u.length>0?function(e){1==n()&&document.addEventListener(i.raw.fullscreenchange,(function(){i.isFullscreen&&e.filter((function(e){e.classList.add(e.dataset.fsToggle)})),i.isFullscreen||e.filter((function(e){e.classList.remove(e.dataset.fsToggle)}))}))}(u):console.log("There are no elements with 'data-fs-toggle'.")):(console.log("The browser does not support the Fullscreen API."),document.body.classList.add("no-fsfx"),o.filter((function(e){!function(e){(1!=r.hideifnofs||e.hasAttribute("data-fs-gonext"))&&(e.style.display="inline-block",e.onclick=function(){t.next()})}(e)})))};return{id:"fsfx",init:function(e){var n=e.getConfig().fsfx||{};!function(e,n){for(var t in n)e.hasOwnProperty(t)||(e[t]=n[t])}(n,{baseclass:"fsbutton",hideifnofs:!0}),t(e,n)}}}}));
+
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global.FsFx = factory());
+}(this, (function () { 'use strict';
+
+  function _slicedToArray(arr, i) {
+    return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
+  }
+
+  function _arrayWithHoles(arr) {
+    if (Array.isArray(arr)) return arr;
+  }
+
+  function _iterableToArrayLimit(arr, i) {
+    if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _e = undefined;
+
+    try {
+      for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+        _arr.push(_s.value);
+
+        if (i && _arr.length === i) break;
+      }
+    } catch (err) {
+      _d = true;
+      _e = err;
+    } finally {
+      try {
+        if (!_n && _i["return"] != null) _i["return"]();
+      } finally {
+        if (_d) throw _e;
+      }
+    }
+
+    return _arr;
+  }
+
+  function _unsupportedIterableToArray(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(o);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+  }
+
+  function _arrayLikeToArray(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+
+    for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+    return arr2;
+  }
+
+  function _nonIterableRest() {
+    throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+  }
+
+  function _createForOfIteratorHelper(o) {
+    if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) {
+      if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) {
+        var i = 0;
+
+        var F = function () {};
+
+        return {
+          s: F,
+          n: function () {
+            if (i >= o.length) return {
+              done: true
+            };
+            return {
+              done: false,
+              value: o[i++]
+            };
+          },
+          e: function (e) {
+            throw e;
+          },
+          f: F
+        };
+      }
+
+      throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+    }
+
+    var it,
+        normalCompletion = true,
+        didErr = false,
+        err;
+    return {
+      s: function () {
+        it = o[Symbol.iterator]();
+      },
+      n: function () {
+        var step = it.next();
+        normalCompletion = step.done;
+        return step;
+      },
+      e: function (e) {
+        didErr = true;
+        err = e;
+      },
+      f: function () {
+        try {
+          if (!normalCompletion && it.return != null) it.return();
+        } finally {
+          if (didErr) throw err;
+        }
+      }
+    };
+  }
+
+  /* eslint-disable promise/prefer-await-to-then */
+  var methodMap = [['requestFullscreen', 'exitFullscreen', 'fullscreenElement', 'fullscreenEnabled', 'fullscreenchange', 'fullscreenerror'], // New WebKit
+  ['webkitRequestFullscreen', 'webkitExitFullscreen', 'webkitFullscreenElement', 'webkitFullscreenEnabled', 'webkitfullscreenchange', 'webkitfullscreenerror'], // Old WebKit
+  ['webkitRequestFullScreen', 'webkitCancelFullScreen', 'webkitCurrentFullScreenElement', 'webkitCancelFullScreen', 'webkitfullscreenchange', 'webkitfullscreenerror'], ['mozRequestFullScreen', 'mozCancelFullScreen', 'mozFullScreenElement', 'mozFullScreenEnabled', 'mozfullscreenchange', 'mozfullscreenerror'], ['msRequestFullscreen', 'msExitFullscreen', 'msFullscreenElement', 'msFullscreenEnabled', 'MSFullscreenChange', 'MSFullscreenError']];
+
+  var nativeAPI = function () {
+    var unprefixedMethods = methodMap[0];
+    var returnValue = {};
+
+    var _iterator = _createForOfIteratorHelper(methodMap),
+        _step;
+
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var methodList = _step.value;
+        var exitFullscreenMethod = methodList === null || methodList === void 0 ? void 0 : methodList[1];
+
+        if (exitFullscreenMethod in document) {
+          var _iterator2 = _createForOfIteratorHelper(methodList.entries()),
+              _step2;
+
+          try {
+            for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+              var _step2$value = _slicedToArray(_step2.value, 2),
+                  index = _step2$value[0],
+                  method = _step2$value[1];
+
+              returnValue[unprefixedMethods[index]] = method;
+            }
+          } catch (err) {
+            _iterator2.e(err);
+          } finally {
+            _iterator2.f();
+          }
+
+          return returnValue;
+        }
+      }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
+
+    return false;
+  }();
+
+  var eventNameMap = {
+    change: nativeAPI.fullscreenchange,
+    error: nativeAPI.fullscreenerror
+  }; // eslint-disable-next-line import/no-mutable-exports
+
+  var screenfull = {
+    // eslint-disable-next-line default-param-last
+    request: function request() {
+      var element = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document.documentElement;
+      var options = arguments.length > 1 ? arguments[1] : undefined;
+      return new Promise(function (resolve, reject) {
+        var onFullScreenEntered = function onFullScreenEntered() {
+          screenfull.off('change', onFullScreenEntered);
+          resolve();
+        };
+
+        screenfull.on('change', onFullScreenEntered);
+        var returnPromise = element[nativeAPI.requestFullscreen](options);
+
+        if (returnPromise instanceof Promise) {
+          returnPromise.then(onFullScreenEntered).catch(reject);
+        }
+      });
+    },
+    exit: function exit() {
+      return new Promise(function (resolve, reject) {
+        if (!screenfull.isFullscreen) {
+          resolve();
+          return;
+        }
+
+        var onFullScreenExit = function onFullScreenExit() {
+          screenfull.off('change', onFullScreenExit);
+          resolve();
+        };
+
+        screenfull.on('change', onFullScreenExit);
+        var returnPromise = document[nativeAPI.exitFullscreen]();
+
+        if (returnPromise instanceof Promise) {
+          returnPromise.then(onFullScreenExit).catch(reject);
+        }
+      });
+    },
+    toggle: function toggle(element, options) {
+      return screenfull.isFullscreen ? screenfull.exit() : screenfull.request(element, options);
+    },
+    onchange: function onchange(callback) {
+      screenfull.on('change', callback);
+    },
+    onerror: function onerror(callback) {
+      screenfull.on('error', callback);
+    },
+    on: function on(event, callback) {
+      var eventName = eventNameMap[event];
+
+      if (eventName) {
+        document.addEventListener(eventName, callback, false);
+      }
+    },
+    off: function off(event, callback) {
+      var eventName = eventNameMap[event];
+
+      if (eventName) {
+        document.removeEventListener(eventName, callback, false);
+      }
+    },
+    raw: nativeAPI
+  };
+  Object.defineProperties(screenfull, {
+    isFullscreen: {
+      get: function get() {
+        return Boolean(document[nativeAPI.fullscreenElement]);
+      }
+    },
+    element: {
+      enumerable: true,
+      get: function get() {
+        var _document$nativeAPI$f;
+
+        return (_document$nativeAPI$f = document[nativeAPI.fullscreenElement]) !== null && _document$nativeAPI$f !== void 0 ? _document$nativeAPI$f : undefined;
+      }
+    },
+    isEnabled: {
+      enumerable: true,
+      // Coerce to boolean in case of old WebKit.
+      get: function get() {
+        return Boolean(document[nativeAPI.fullscreenEnabled]);
+      }
+    }
+  });
+
+  if (!nativeAPI) {
+    screenfull = {
+      isEnabled: false
+    };
+  }
+
+  var screenfull$1 = screenfull;
+
+  var Plugin = function Plugin() {
+    var selectionArray = function selectionArray(container, selectors) {
+      var selections = container.querySelectorAll(selectors);
+      var selectionarray = Array.prototype.slice.call(selections);
+      return selectionarray;
+    };
+
+    var sfEnabled = function sfEnabled() {
+      if (screenfull$1.enabled || screenfull$1.isEnabled) {
+        return true;
+      }
+
+      return false;
+    };
+
+    var fullScreenEffects = function fullScreenEffects(deck, options) {
+      var viewport = deck.getRevealElement().tagName == "BODY" ? document : deck.getRevealElement();
+      var fsButtons = selectionArray(viewport, ".".concat(options.baseclass));
+      var toggleThese = selectionArray(document, "[data-fs-toggle]");
+
+      var hideIfNoFS = function hideIfNoFS(fsButton) {
+        if (options.hideifnofs == true && !fsButton.hasAttribute("data-fs-gonext")) ; else {
+          fsButton.style.display = "inline-block";
+
+          fsButton.onclick = function () {
+            deck.next();
+          };
+        }
+      };
+
+      var buttonCheck = function buttonCheck(fsButtons) {
+        fsButtons.filter(function (fsButton) {
+          fsButton.style.display = "inline-block";
+
+          var goNext = function goNext() {
+            if (parseInt(fsButton.dataset.fsGonext) > 0 && !screenfull$1.isFullscreen) {
+              setTimeout(function () {
+                deck.next();
+              }, parseInt(fsButton.dataset.fsGonext));
+            } else {
+              deck.next();
+            }
+          };
+
+          fsButton.onclick = function () {
+            if (sfEnabled() == true) {
+              if (fsButton.hasAttribute("data-fs-gonext")) {
+                if (screenfull$1.isFullscreen) {
+                  goNext();
+                } else {
+                  screenfull$1.request(viewport).then(goNext());
+                  element.requestFullscreen();
+                }
+              } else {
+                screenfull$1.toggle(viewport);
+              }
+            } else {
+              deck.next();
+            }
+          };
+        });
+      };
+
+      var toggleCheck = function toggleCheck(toggleThese) {
+        var fullscreenchange = function fullscreenchange() {
+          if (screenfull$1.isFullscreen) {
+            toggleThese.filter(function (toggleThis) {
+              toggleThis.classList.add(toggleThis.dataset.fsToggle);
+            });
+          }
+
+          if (!screenfull$1.isFullscreen) {
+            toggleThese.filter(function (toggleThis) {
+              toggleThis.classList.remove(toggleThis.dataset.fsToggle);
+            });
+          }
+        };
+
+        if (sfEnabled() == true) {
+          document.addEventListener(screenfull$1.raw.fullscreenchange, fullscreenchange);
+        }
+      };
+
+      if (!sfEnabled()) {
+        console.log("The browser does not support the Fullscreen API.");
+        document.body.classList.add("no-fsfx");
+        fsButtons.filter(function (fsButton) {
+          hideIfNoFS(fsButton);
+        });
+      } else {
+        if (fsButtons.length > 0) {
+          buttonCheck(fsButtons);
+        } else {
+          console.log("There are no FS buttons");
+        }
+
+        if (toggleThese.length > 0) {
+          toggleCheck(toggleThese);
+        } else {
+          console.log("There are no elements with 'data-fs-toggle'.");
+        }
+      }
+    };
+
+    var init = function init(deck) {
+      var defaultOptions = {
+        baseclass: 'fsbutton',
+        hideifnofs: true
+      };
+
+      var defaults = function defaults(options, defaultOptions) {
+        for (var i in defaultOptions) {
+          if (!options.hasOwnProperty(i)) {
+            options[i] = defaultOptions[i];
+          }
+        }
+      };
+
+      var options = deck.getConfig().fsfx || {};
+      defaults(options, defaultOptions);
+      fullScreenEffects(deck, options);
+    };
+
+    return {
+      id: 'fsfx',
+      init: init
+    };
+  };
+
+  return Plugin;
+
+})));
